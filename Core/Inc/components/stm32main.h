3fac34d1ce3c726d5e7d56a9672b5099aff51545
Xavier Ullastre Busca <xullasbu7@alumnes.ub.edu>
2023-05-23 13:46:33 +0200

    STM32main files created

---
+++ Core/Inc/components/stm32main.h
@@ -0,0 +1,13 @@
+/*
+ * stm32main.h
+ *
+ *  Created on: 23 may. 2023
+ *      Author: XAVIER
+ */
+
+#ifndef INC_COMPONENTS_STM32MAIN_H_
+#define INC_COMPONENTS_STM32MAIN_H_
+
+
+
+#endif /* INC_COMPONENTS_STM32MAIN_H_ */
---
+++ Core/Src/components/stm32main.c
@@ -0,0 +1,8 @@
+/*
+ * stm32main.c
+ *
+ *  Created on: 23 may. 2023
+ *      Author: XAVIER
+ */
+
+
