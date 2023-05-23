fa6e493851923d4d49b7b31bfc5152e53e52ac82
Antonio Javier Gomez Asencio <xgomezasencio@gmail.com>
2023-05-23 14:56:06 +0200

    Timer sampling period defined.

--- Core/Src/components/timers.c
+++ Core/Src/components/timers.c
@@ -6,3 +6,16 @@
  */
 
 
+MX_TIM2_Init();
+HAL_TIM_Base_Start_IT(&htim2);
+
+HAL_TIM_SET_AUTORELOAD(TIM_HandleTypeDef*htim2, Sampling_Period*10)
+
+void HAL_TIMPeriodElapsedCallback(TIM_HandleTypeDef*htim){
+	Sampling_Period_Completed = TRUE;
+
+}
+
+void Sample_Period_Ellaspsed(void){
+	Sampling_Period_Completed = FALSE;
+}
