3a78d95fce6614618e2c94c78bbb16c7985ba27b
Xavier Ullastre Busca <xullasbu7@alumnes.ub.edu>
2023-05-29 11:29:13 +0200

    PMU implemented

---
+++ Core/Inc/components/PMU.h
@@ -0,0 +1,17 @@
+/*
+ * PMU.h
+ *
+ *  Created on: May 29, 2023
+ *      Author: XAVIER
+ */
+
+#ifndef INC_COMPONENTS_PMU_H_
+#define INC_COMPONENTS_PMU_H_
+
+#define EN_Pin GPIO_PIN_5
+#define EN_GPIO_Port GPIOA
+
+#include "stm32f4xx_hal.h"
+
+
+#endif /* INC_COMPONENTS_PMU_H_ */
