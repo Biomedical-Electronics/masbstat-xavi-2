03a7fd52378516160d936f2ca8cb4449562c9c2b
Antonio Javier Gomez Asencio <xgomezasencio@gmail.com>
2023-05-30 13:46:07 +0200

    ADC files created.

---
+++ Core/Inc/components/adc.h
@@ -0,0 +1,13 @@
+/*
+ * adc.h
+ *
+ *  Created on: 30 may. 2023
+ *      Author: xgome
+ */
+
+#ifndef INC_COMPONENTS_ADC_H_
+#define INC_COMPONENTS_ADC_H_
+
+
+
+#endif /* INC_COMPONENTS_ADC_H_ */
---
+++ Core/Src/components/adc.h
@@ -0,0 +1,8 @@
+/*
+ * adc.h
+ *
+ *  Created on: 30 may. 2023
+ *      Author: xgome
+ */
+
+
