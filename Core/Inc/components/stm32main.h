197965a662f44c6228a04a0b8b129c44a7f9c266
Antonio Javier Gomez Asencio <xgomezasencio@gmail.com>
2023-05-30 14:19:07 +0200

    Setup and loop functions started.

--- Core/Inc/components/stm32main.h
+++ Core/Inc/components/stm32main.h
@@ -8,6 +8,19 @@
 #ifndef INC_COMPONENTS_STM32MAIN_H_
 #define INC_COMPONENTS_STM32MAIN_H_
 
+#include "main.h"
+#include "components/PMU.h"
+#include "components/mcp4725_driver.h"
 
+struct Handles_S {
+    UART_HandleTypeDef *huart;
+    // Aqui iriamos anadiendo los diferentes XXX_HandleTypeDef que necesitaramos anadir.
+};
+
+
+
+//Prototypes
+void setup(struct Handles_S *handles);
+void loop(void);
 
 #endif /* INC_COMPONENTS_STM32MAIN_H_ */
