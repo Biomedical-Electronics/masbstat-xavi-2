49bcdb24b6e1f072e39d8ff8094999a6d686f94b
Xavier Ullastre Busca <xullasbu7@alumnes.ub.edu>
2023-06-05 18:31:22 +0200

    ADC functions created

--- Core/Inc/components/adc.h
+++ Core/Inc/components/adc.h
@@ -8,6 +8,15 @@
 #ifndef INC_COMPONENTS_ADC_H_
 #define INC_COMPONENTS_ADC_H_
 
+#include "main.h"
+#include "formulas.h"
+
+ double V_ref = 0;
+ double I_cell = 0;
+ extern ADC_HandleTypeDef hadc1;
+
+double get_V_CA(void);
+double get_I_CA(void);
 
 
 #endif /* INC_COMPONENTS_ADC_H_ */
--- Core/Src/components/adc.c
+++ Core/Src/components/adc.c
@@ -5,4 +5,22 @@
  *      Author: XAVIER
  */
 
+#include "components/adc.h"
 
+
+
+double get_V_CA(void){
+	HAL_ADC_Start(&hadc1); // iniciamos la conversion para el divisor de tension
+	HAL_ADC_PollForConversion(&hadc1, 200); // esperamos que finalice la conversion    																				// si en 200 ms no termina, salimos
+	V_ref = calculateVrefVoltage(HAL_ADC_GetValue(&hadc1));
+	return V_ref;
+
+}
+
+double get_I_CA(void){
+	HAL_ADC_Start(&hadc1); // iniciamos la conversion para el divisor de tension
+	HAL_ADC_PollForConversion(&hadc1, 200); // esperamos que finalice la conversion  																				// si en 200 ms no termina, salimos
+	I_cell = calculateIcellCurrent(HAL_ADC_GetValue(&hadc1));
+	return I_cell;
+
+}
